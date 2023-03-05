978028b0f8725724b6a0e0ba20d9f5632eb45c28
SantGT5 <gianspf@gmail.com>
2023-03-05 01:28:16 +0100

    docker compose

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,12 +1,17 @@
-import mongoose, { connect, disconnect } from 'mongoose';
+import mongoose, { connect, disconnect, ConnectOptions } from 'mongoose';
 
 import 'dotenv/config';
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/graphqlTodo';
+const mongodbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+} as ConnectOptions;
+
+const MONGO_URL = 'mongodb://mongo:27017/graphqlTodo';
 
 mongoose.connection.once('open', () => {
     // eslint-disable-next-line no-console
-    console.log('🪐 MongoDB connection ready!');
+    console.log('🪐 MongoDB ready at http://localhost:8081/');
 });
 
 mongoose.connection.on('error', (err) => {
@@ -14,7 +19,9 @@ mongoose.connection.on('error', (err) => {
 });
 
 async function mongoConnect() {
-    await connect(MONGO_URL);
+    await connect(MONGO_URL, mongodbOptions).catch(() => {
+        process.exit(-1);
+    });
 }
 
 async function mongoDisconnect() {
