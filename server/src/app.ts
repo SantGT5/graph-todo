2f475076bc6c1cd2214491c69c3fc62f9f1d9d73
SantGT5 <gianspf@gmail.com>
2023-04-02 15:52:57 +0200

    helmet express secure

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import type { BaseContext } from '@apollo/server';
 
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 // Apollo
 import { ApolloServer } from '@apollo/server';
@@ -18,11 +19,11 @@ const app: Express = express();
 app.use(
     cors({
         origin: ['http://localhost:3050', 'https://studio.apollographql.com'],
-        credentials: true,
         optionsSuccessStatus: 200,
     })
 );
 
+app.use(helmet());
 app.use(express.json());
 
 async function startApolloServer(
